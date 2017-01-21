ac09308703fcc50d75868c65ace61511280fdbed
Thor Kell <thor@tide-pool.ca>
2017-01-20 23:01:49 -0500

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 echo "beginning rsync"
-rsync -r --exclude 'deploy.sh' --exclude '.git' --exclude 'node_modules' --exclude 'my.conf.js' --exclude 'npm-debug.log' /home/thor/Code/nothingsoundsquitelikean/* tidepool@tide-pool.ca:/home/tidepool/www/nothingsoundsquitelikean
+rsync -i /Users/thor/.ssh/dreamhost_rsa -r --exclude 'deploy.sh' --exclude '.git' --exclude 'node_modules' --exclude 'my.conf.js' --exclude 'npm-debug.log' /Users/thor/Code/every909/* tidepool@tide-pool.ca:/home/tidepool/www/every909
 echo "rsync complete!"
