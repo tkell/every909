ef003bb967b1ea455923463f93adf2c8fc3e9aac
Brian Whitman <brian@variogr.am>
2017-02-19 07:41:15 -0500

    tap to start on iOS

--- js/controllers.js
+++ js/controllers.js
@@ -178,3 +178,18 @@ sequenceApp.controller('SequencerControl', function ($scope, $http, $timeout) {
         return source
     }
 })
+
+window.addEventListener('touchstart', function() {
+
+    // create empty buffer
+    var buffer = myContext.createBuffer(1, 1, 22050);
+    var source = myContext.createBufferSource();
+    source.buffer = buffer;
+
+    // connect to output (your speakers)
+    source.connect(myContext.destination);
+
+    // play the file
+    source.noteOn(0);
+
+}, false);
